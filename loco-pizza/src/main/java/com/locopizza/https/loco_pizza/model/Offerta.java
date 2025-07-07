a0210a339ffa412996c2dba4f7cd626adc9fd74f
topqualityconsulenza <marketing@consulenzatopquality.com>
2025-07-07 10:16:38 +0200

    creazione entità offerta

--- loco-pizza/src/main/java/com/locopizza/https/loco_pizza/model/Offerta.java
+++ loco-pizza/src/main/java/com/locopizza/https/loco_pizza/model/Offerta.java
@@ -1,5 +1,75 @@
 package com.locopizza.https.loco_pizza.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="discounts")
 public class Offerta {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Il titolo è obbligatorio")
+    @Size(max = 100, message = "Il titolo non può superare i 100 caratteri")
+    @Column(name="titles")
+    private String titolo;
+
+    @Column(name="starts_data")
+    private String dataInizio;
+
+    @Column(name="ends_data")
+    private String dataFine;
+
+
+
+    //Get + Set
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitolo() {
+        return this.titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getDataInizio() {
+        return this.dataInizio;
+    }
+
+    public void setDataInizio(String dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public String getDataFine() {
+        return this.dataFine;
+    }
+
+    public void setDataFine(String dataFine) {
+        this.dataFine = dataFine;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+
+
 }
