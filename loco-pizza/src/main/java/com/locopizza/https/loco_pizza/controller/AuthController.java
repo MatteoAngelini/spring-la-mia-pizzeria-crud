b2bc4b03f54d2b079d95dc92428fe9f33a349730
topqualityconsulenza <marketing@consulenzatopquality.com>
2025-07-08 12:10:16 +0200

    Implementazione view e controller per pagina di login personalizzata

---
+++ loco-pizza/src/main/java/com/locopizza/https/loco_pizza/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.locopizza.https.loco_pizza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String index(Model model) {
+        return "/auth/login"; 
+    }
+}
