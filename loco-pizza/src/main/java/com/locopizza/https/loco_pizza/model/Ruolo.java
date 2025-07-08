87f32b918b9b757c55dccd4c919d2224160a5482
topqualityconsulenza <marketing@consulenzatopquality.com>
2025-07-08 12:00:56 +0200

    implementazioni dipendenze spring security e creazione entitù, utente e ruolo

---
+++ loco-pizza/src/main/java/com/locopizza/https/loco_pizza/model/Ruolo.java
@@ -0,0 +1,38 @@
+package com.locopizza.https.loco_pizza.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Ruolo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nome;
+
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    
+}
---
+++ loco-pizza/src/main/java/com/locopizza/https/loco_pizza/model/Utente.java
@@ -0,0 +1,95 @@
+package com.locopizza.https.loco_pizza.model;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String eMail;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String cognome;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    @ManyToMany( fetch = FetchType.EAGER)
+    private Set<Ruolo> ruoli;
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEMail() {
+        return this.eMail;
+    }
+
+    public void setEMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Ruolo> getRuoli() {
+        return this.ruoli;
+    }
+
+    public void setRuoli(Set<Ruolo> ruoli) {
+        this.ruoli = ruoli;
+    }
+    
+}
